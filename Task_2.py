f3d40d06da46e9fa4b5aec77f9cc5b7930081464
Ilya <St@inbox.ru>
2024-03-06 18:21:28 +0300

    Final 01

--- Task_2.py
+++ Task_2.py
@@ -5,7 +5,7 @@ from typing import List
 import pytest
 import shutil
 
-
+#
 def parse_path(path: str) -> List[namedtuple]:
     Object = namedtuple('Object', ['name_file', 'ext', 'full_path', 'directory'])
     return [Object(
@@ -17,7 +17,7 @@ def parse_path(path: str) -> List[namedtuple]:
 #
 def gen_file_data(dir_path):
     pass
-
+#
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Command line args parser')
     parser.add_argument('dir_path', default='.', type=str,
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     res = gen_file_data(line_args.dir_path)
     print(res)
 
-
+#
 @pytest.fixture
 def temp_dir():
     os.mkdir('test_dir')
@@ -40,24 +40,24 @@ def temp_dir():
     yield d_name
     shutil.rmtree(d_name)
 
-
+#
 @pytest.fixture
 def file_data():
     return namedtuple('Object',
                       ['name_file', 'ext', 'full_path', 'directory'])
 
-
+#
 def test_gen_file_data_correct_file_name(temp_dir, file_data):
     res = parse_path(temp_dir)
     expected = file_data(name_file='a', ext='', full_path=True, directory='test_dir')
     assert res[0].name_file == expected.name_file
 
-
+#
 def test_gen_file_data_correct_parent_dir(temp_dir, file_data):
     res = parse_path(temp_dir)
     expected = file_data(name_file='b', ext='', full_path=True, directory='test_dir')
     assert res[1].directory == expected.directory
 
-
+#
 def test_gen_file_data_correct_size(temp_dir, file_data):
     assert len(parse_path(temp_dir)) == 4
